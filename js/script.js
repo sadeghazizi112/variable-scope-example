227a7ab080690502b8f3b98f191515405775b908
Sadegh Azizi <sadeghazizi112@yahoo.com>
2018-10-03 13:39:21 -0700

    committing this

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $("div#click-one").click(function(event) {
+    whatToSay = "Hello!";
+    alert(whatToSay);
+  });
+
+  $("div#click-two").click(function(event) {
+    alert(whatToSay);
+  });
+});
